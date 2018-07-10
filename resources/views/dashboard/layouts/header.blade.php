244a63735dea3579042761332dfe8f4ed2c60e73
Mohamed Masoud <luca.110077@gmail.com>
2018-07-10 11:13:54 +0200

    more fetures

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-    <meta name="author" content="Coderthemes">
+    <meta name="author" content="Mohamed Masoud">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="shortcut icon" href="{{asset('images/site/logo.png')}}">
